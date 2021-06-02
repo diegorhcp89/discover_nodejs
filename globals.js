a6fb139d7aad28539570ca36e26bba98fd51490d
Diego <diegoferreiradoamaral@gmail.com>
2021-06-01 22:04:19 -0300

    Estudando NodeJs

---
+++ globals.js
@@ -0,0 +1,5 @@
+// Diretorio local, onde está o aquivo que estamos executando
+console.log(__dirname)
+
+// Caminho completo + nome do arquivo
+console.log(__filename)
---
+++ process.js
@@ -0,0 +1,11 @@
+// lista de argumentos
+// console.log(process.argv)
+
+// console.log('seu nome é ', process.argv[2] + '' + process.argv[3])
+
+// console.log(`Seu nome é ${process.argv[2]} ${process.argv[3]}`)
+
+const firstName = process.argv[2]
+const lastName = process.argv[3]
+
+console.log(`Seu nome é ${firstName} ${lastName}`)
